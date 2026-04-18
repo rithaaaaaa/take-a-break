df3fab9636a750007d2b383bee8d03dbe1da36f3
rithaaaaaa <nagubandiasritha30@gmail.com>
2026-04-18 11:03:40 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = "take-a-break-v1";
+const ASSETS = ["/", "/index.html", "/app.js", "/manifest.json", "/icons/icon192.png", "/icons/icon512.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
+
+self.addEventListener("notificationclick", e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow("/"));
+});
+
+self.addEventListener("push", e => {
+  e.waitUntil(self.registration.showNotification("☕ Take a Break!", {
+    body: "Time to learn something new!",
+    icon: "/icons/icon192.png"
+  }));
+});
